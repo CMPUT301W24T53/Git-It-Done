c22aa42c89dcb7d1bbd73f3bedb9e1629dbe9b21
KevinOLearyVEVO <dylancpaton2@gmail.com>
2024-03-07 01:07:48 -0700

    Created Skeleton Xml for the attendee profile page

--- app/src/main/java/com/example/gidevents/ProfileEditActivity.java
+++ app/src/main/java/com/example/gidevents/ProfileEditActivity.java
@@ -1,4 +1,25 @@
 package com.example.gidevents;
 
-public class ProfileEditActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileEditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_profile);
+
+        Button UsernameBtn = findViewById(R.id.upUsernameButton);
+        Button NameBtn = findViewById(R.id.upNameButton);
+        Button EmailBtn = findViewById(R.id.upEmailButton);
+        Button PhoneBtn = findViewById(R.id.upPhoneButton);
+        Button GenderBtn = findViewById(R.id.upGenderButton);
+        Button BirthdayBtn = findViewById(R.id.upBirthdayButton);
+        Button AddressBtn = findViewById(R.id.upAddressButton);
+        Button GeoLocBtn = findViewById(R.id.upGeoLocationButton);
+    }
+
 }

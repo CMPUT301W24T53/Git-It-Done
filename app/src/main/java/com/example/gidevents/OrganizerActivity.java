66becadbdc718761d10017d7b2a794b4c62e2b08
Abubakar Mir <abubakarmir21@gmail.com>
2024-03-23 10:40:26 -0600

    rebase abu onto main

--- app/src/main/java/com/example/gidevents/OrganizerActivity.java
+++ app/src/main/java/com/example/gidevents/OrganizerActivity.java
@@ -1,4 +1,27 @@
 package com.example.gidevents;
 
-public class OrganizerActivity {
-}
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class OrganizerActivity  extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.);
+
+
+
+
+
+        }
+    }
